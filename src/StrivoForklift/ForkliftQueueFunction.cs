8eea686b9c712f49ef274172aaefeba2a070f88c
Karl Fridlund <karl.fridlund@strivo.se>
2026-03-20 12:37:28 +0100

    Ändra queue name

--- src/StrivoForklift/ForkliftQueueFunction.cs
+++ src/StrivoForklift/ForkliftQueueFunction.cs
@@ -25,7 +25,7 @@ public class ForkliftQueueFunction
 
     [Function(nameof(ForkliftQueueFunction))]
     public async Task Run(
-        [QueueTrigger("consumethis", Connection = "StorageQueue")] string rawMessage)
+        [QueueTrigger("karl-queue", Connection = "StorageQueue")] string rawMessage)
     {
         _logger.LogInformation("Dequeued raw message: {RawMessage}", rawMessage);
 
